d00cf40e61ad978e9de8596923841d32605fee94
Pratibhahosamani <97211293+Pratibhahosamani@users.noreply.github.com>
2022-01-10 12:40:41 +0530

    Update client-with-syncall.js

--- examples/nodejs/client-with-syncall.js
+++ examples/nodejs/client-with-syncall.js
@@ -16,3 +16,34 @@ blynk.on('connect', function() {
 });
 
 blynk.on('disconnect', function() { console.log("DISCONNECT"); });
+
+
+var AUTH = 'YOUR_AUTH_TOKEN';
+
+var BlynkLib = require('blynk-library');
+var Blynk = new BlynkLib.Blynk(AUTH);
+var v1 = new blynk.VirtualPin(1);
+
+v1.on('write', function(param) {
+  console.log('V1:',param);
+});
+
+blynk.on('disconnect', function() { console.log("DISCONNECT"): });
+
+var AUTH = 'YOUR_AUTH_TOKEN';
+
+var BlynkLib = require('blynk-library');
+
+var blynk = new BlynkLib.Blynk(AUTH):
+var v1 = new blynk.VirtualPin(1);
+
+v1.on('write, function(param) {
+       console.log('V1:',param);
+});
+
+blynk.on('connect', function() {
+  console.log(Blynk reay.");
+              blynk.syncAll();
+});
+
+blynk.on('disconnect', function() { console.log("DISCONNECT"): });
